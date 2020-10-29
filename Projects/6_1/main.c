b1cdef81ac5abff461ed92ea651315ad749a9bc3
vovatatarinov <vovatatarinov@bk.ru>
2020-10-29 21:28:02 +0300

    Бэкап 6-ой задачи Изменения, которые будут включены в коммит: 	изменено:      Projects/6_1/main.c 	изменено:      Projects/6_1/settings/6_1.dbgdt 	изменено:      settings/STM32F429Workspace.wsdt

--- Projects/6_1/main.c
+++ Projects/6_1/main.c
@@ -4,19 +4,31 @@
 #include "stm32f4xx_ll_gpio.h"
 #include "stm32f4xx_ll_rcc.h"
 
-static short c;
-void SPI5_IRQHandler() {
-    c = LL_SPI_ReceiveData16(SPI5);
-}
 
 void delay(int ms) {
-    
+    for (volatile int i = 0; i < 16000; ++i) {
+      for (volatile int j = 0; j < ms; ++j) {
+        ;
+      }
+    }
+}
+
+
+static short c[8];
+static int var = 0;
+void SPI5_IRQHandler() {
+  if (var < 8) {
+    c[var] = LL_SPI_ReceiveData16(SPI5);
+    //delay(10);
+    ++var;
+    while (LL_SPI_IsActiveFlag_RXNE(SPI5));
+  }
 }
 
 int main(){
     /* RCC для GPIOB и SPI2 */
     LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOF);
-    LL_APB1_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SPI5);
+    LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SPI5);
     /* Альтернативная функция для GPIO */
     //LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_12, LL_GPIO_MODE_ALTERNATE);
     LL_GPIO_SetPinMode(GPIOF, LL_GPIO_PIN_6, LL_GPIO_MODE_OUTPUT);
@@ -32,25 +44,33 @@ int main(){
     LL_SPI_SetClockPhase(SPI5,  LL_SPI_PHASE_2EDGE);
     LL_SPI_SetClockPolarity(SPI5,  LL_SPI_POLARITY_HIGH);
 
-    LL_SPI_SetBaudRatePrescaler(SPI5, LL_SPI_BAUDRATEPRESCALER_DIV2);
+    LL_SPI_SetBaudRatePrescaler(SPI5, LL_SPI_BAUDRATEPRESCALER_DIV8);
     LL_SPI_SetTransferBitOrder(SPI5, LL_SPI_MSB_FIRST);
     LL_SPI_SetTransferDirection(SPI5,  LL_SPI_FULL_DUPLEX);
     LL_SPI_SetDataWidth(SPI5, LL_SPI_DATAWIDTH_16BIT);
     LL_SPI_DisableCRC(SPI5);
     LL_SPI_SetNSSMode(SPI5, LL_SPI_NSS_SOFT);
-    //LL_SPI_SetNSSMode(SPI2, LL_SPI_NSS_HARD_INPUT);
     LL_SPI_EnableIT_RXNE(SPI5);
     LL_SPI_Enable(SPI5);
+    LL_GPIO_ResetOutputPin(GPIOF, (1 << 6));
+    LL_SPI_TransmitData16(SPI5, 0x200F);
+        //delay(10);
+    LL_SPI_ReceiveData16(SPI5);
+    while (LL_SPI_IsActiveFlag_TXE(SPI5));
+    LL_GPIO_SetOutputPin(GPIOF, (1 << 6));
     NVIC_EnableIRQ(SPI5_IRQn);
     /* prepare packet */
-    short pack =  (0x0F << 8) | 0x0001;
-        LL_GPIO_ResetOutputPin(GPIOF, (1 << 6));
-    LL_SPI_TransmitData16(SPI5, pack);
-    while (LL_SPI_IsActiveFlag_TXE(SPI5));
+    short pack =  0x8F;
+    
+    //pack =  1 | (0x0F << 2);
     //LL_GPIO_SetOutputPin(GPIOF, (1 << 6));
     while(1) {
-
-        
+        LL_GPIO_ResetOutputPin(GPIOF, (1 << 6));
+        LL_SPI_TransmitData16(SPI5, pack);
+        //delay(10);
+        while (LL_SPI_IsActiveFlag_TXE(SPI5));
+        LL_GPIO_SetOutputPin(GPIOF, (1 << 6));
+        delay(10);
     }
 }
 
